48b39a3864908c106cd547ad9197b8460b86fee9
Yen Ching Tan <yc12.tan@gmail.com>
2020-04-05 15:45:28 -0700

    added more js stuff

--- src/ca/ubc/cs304/javascript/Home.js
+++ src/ca/ubc/cs304/javascript/Home.js
@@ -1,32 +1,42 @@
-const sin = $('#sin');
+// const sin = $('#SIN');
+// const year = $('#Year');
+// const major = $('#Major');
+// const fname = $('#fn');
+// const lname = $('#ln');
+// const address = $('#address');
 
-<input id="sin"> </input>
+// <input id="sin"> </input>
 
-let sin = 1;
-let year = 3;
+let sin = document.getElementById("sinField");
+let year = document.getElementById("yearField");
+let major = document.getElementById("majorField");
+let fname = document.getElementById("fnfield");
+let lname = document.getElementById("lnfield");
+let address = document.getElementById("addressfield");
 
-$('#new-app').click(() => {
+$('#addButton').click(() => {
     const submit = $.ajax({
         type: 'POST',
-        url: '/applicant/create/sin/{year}/{major}/{fname}/{lname}/{address}'
+        url: '/applicant/create/sin/year/major/fname/lname/address'
     });
 
     submit.done(function (result) {
         if (result === "Success") {
-            $('#text-bubble').text(`Successfully added in application with name ${fname} and ${lname}!`);
+ //           $('#text-bubble').text(`Successfully added in applicant with name ${fname} ${lname}!`);
+            alert("Successfully added in applicant with name ${fname} ${lname}!");
         }
         else {
             $('#text-bubble').text("Unsuccessful");
         }
     });
 
-    submit.fail() {
-        $('#text-bubble').text("Unsuccessful");
-    }
+    // submit.fail(){
+    //     $('#text-bubble').text("Unsuccessful");
+    // }
 });
 
-for (let res : result) {
-    let jobNo = res['jobNo']; // 1000
-    let jobTitle = res['jobTitle'];
-    let description = res['description'];
-}
+// for (let res : result) {
+//     let jobNo = res['jobNo']; // 1000
+//     let jobTitle = res['jobTitle'];
+//     let description = res['description'];
+// }
